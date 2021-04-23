a944ea5f83d3ebad7265e54822fe92104922f72f
clarade <cdesperben@yahoo.fr>
2021-04-23 09:48:38 +0200

    finished display of series in show and index pages about categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Category;
+use App\Entity\Program;
 
 class CategoryController extends AbstractController
 {
@@ -14,5 +15,37 @@ class CategoryController extends AbstractController
      */
     public function index(): Response
     {
+        $categories = $this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findAll();
+
+        return $this->render(
+            'category/index.html.twig',
+            ['categories' => $categories]
+        );
+    }
+
+    /**
+     * @Route("/categories/{categoryName}", name="category_show")
+     */
+
+    public function show(string $categoryName):Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $categoryName]);
+    
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category with name : '.$categoryName.' found in category table.'
+            );
+        }
+        $programs = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findBy(['category' => $category->getId()], ['id' => 'DESC'], 3);
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category, 'programs' => $programs
+        ]);
     }
 }
