3a13a9f21d531dba7f534ba6143dc24c9e724e0e
clarade <cdesperben@yahoo.fr>
2021-04-29 23:21:13 +0200

    changed database, styled list in show season file

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
+use App\Entity\Season;
 
 class ProgramController extends AbstractController
 {
@@ -31,15 +32,50 @@ class ProgramController extends AbstractController
     {
         $program = $this->getDoctrine()
             ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
+            ->find($id);
     
         if (!$program) {
             throw $this->createNotFoundException(
                 'No program with id : '.$id.' found in program\'s table.'
             );
         }
+
+        $seasons = $program->getSeasons();
+
         return $this->render('program/show.html.twig', [
             'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+
+    /**
+     * @Route("programs/{programId}/seasons/{seasonId}", methods={"GET"}, requirements={"programId"="\d+", "seasonId"="\d+"}, name="program_season_show")
+     */
+    public function showSeason(int $programId, int $seasonId)
+    {
+        $program = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->find($programId);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$programId.' found in program\'s table.'
+            );
+        }
+
+        $season = $this->getDoctrine()
+        ->getRepository(Season::class)
+        ->find($seasonId);
+
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season with id : '.$seasonId.' found in season\'s table.'
+            );
+        }
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
         ]);
     }
 }
